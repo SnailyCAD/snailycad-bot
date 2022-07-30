9b23f0be960a585455778bcbfd495d6bd3001edd
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-30 15:12:51 +0200

    :hammer: chore: add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
+import * as Sentry from "@sentry/node";
 import { Bot } from "./structures/Bot.js";
 
 const bot = new Bot();
 
-bot.login(process.env["BOT_TOKEN"]);
+await bot.login(process.env["BOT_TOKEN"]);
+Sentry.init({
+  dsn: "https://5152dff1b7334743b67ee49ae394aee6@o518232.ingest.sentry.io/6613141",
+  tracesSampleRate: 1.0,
+});
